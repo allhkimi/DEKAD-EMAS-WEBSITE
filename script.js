4c357e9d9b3ac42e4413e4d32c3be40bb984bd18
Al Hakimi <alhakimihaikal4106@gmail.com>
2025-08-19 10:30:38 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+e.preventDefault();
+alert('Thank you for contacting Dekad Emas Resources! We will get back to you soon.');
+this.reset();
+});
